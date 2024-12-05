aad5d90f6e7feb155b4de2dd04d8215d7449eecf
Justin Gardner <justin@rhynorater.com>
2024-12-05 06:02:59 -0500

    1.4.13

--- pinterest-for-woocommerce/vendor/composer/installed.php
+++ pinterest-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/pinterest-for-woocommerce',
-        'pretty_version' => 'dev-release/1.4.12',
-        'version' => 'dev-release/1.4.12',
-        'reference' => 'fb567c82d6f76d8b28c66e39704d90c23a27f674',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'fb1c40e5bf607f29c5a1ea18f8faf09562a9f160',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -56,9 +56,9 @@
             'dev_requirement' => false,
         ),
         'woocommerce/pinterest-for-woocommerce' => array(
-            'pretty_version' => 'dev-release/1.4.12',
-            'version' => 'dev-release/1.4.12',
-            'reference' => 'fb567c82d6f76d8b28c66e39704d90c23a27f674',
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'fb1c40e5bf607f29c5a1ea18f8faf09562a9f160',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
